851ad37b94c7f745e67fdf7a7d4a823bb3860fe0
Jim Alexander <jim@drilltec.local>
2018-10-11 10:17:17 +1100

    outline content is visible for jim only.

--- src/Pages/Outline.js
+++ src/Pages/Outline.js
@@ -12,7 +12,7 @@ class Outline extends React.Component {
     this.state = {
       currentRecord: null,
       loadedComments: null,
-      once:null
+      once: null
     };
     this.setRecord = this.setRecord.bind(this);
 
@@ -28,15 +28,15 @@ class Outline extends React.Component {
     }
   }
   updateRecordComments(value, original) {
-    if(!original) {
-      message.error('Please select a job.'); 
+    if (!original) {
+      message.error('Please select a job.');
     } else {
 
       var record = original;
       record.form_values['0bd7'] = value;
-  
+
       fulcrumRecordUpdate(record.id, record);
-  
+
       function fulcrumRecordUpdate(id, obj) {
         message.loading('Updating form..', 2)
         console.log(id, obj);
@@ -44,17 +44,17 @@ class Outline extends React.Component {
           .then((record) => {
             console.log('success', record);
             message.success('Comments have been set to: ' + value, 2.5)
-  
+
           })
           .catch((error) => {
             console.log(error.message);
           });
-  
+
       }
     }
 
   };
-  
+
   learnWebhooks() {
     // if (this.state.once === null) {
     //   const obj = {
@@ -62,7 +62,7 @@ class Outline extends React.Component {
     //     url: 'http://drilltec.com.au/client-portal/#/',
     //     active: true
     //   };
-       
+
     //   client.webhooks.create(obj)
     //     .then((webhook) => {
     //       console.log('success', webhook);
@@ -73,7 +73,7 @@ class Outline extends React.Component {
     //       console.log(error.message);
     //     });
     // }
-    
+
   }
 
   recordList() {
@@ -85,21 +85,26 @@ class Outline extends React.Component {
   }
 
   render() {
+    if (this.props.user.username === 'Jim Alexander') {
+      return (
+        <div>
+          <h1>Comments</h1>
+          <h3>This can add and edit comments on the Simpson construction test app on fulcrum</h3>
+          {this.recordList()}
+          <Search
+            placeholder="Input Comments here."
+            enterButton="Save"
+            defaultValue={this.state.loadedComments}
+            size="large"
+            onSearch={value => this.updateRecordComments(value, this.state.currentRecord)}
+          />
+
+        </div>
+      )
+    }
     this.learnWebhooks()
     return (
-      <div>
-        <h1>Comments</h1>
-        <h3>This can add and edit comments on the Simpson construction test app on fulcrum</h3>
-        {this.recordList()}
-        <Search
-          placeholder="Input Comments here."
-          enterButton="Save"
-          defaultValue={this.state.loadedComments}
-          size="large"
-          onSearch={value => this.updateRecordComments(value, this.state.currentRecord)}
-        />
-
-      </div>
+      <div />
     )
   }
 }
