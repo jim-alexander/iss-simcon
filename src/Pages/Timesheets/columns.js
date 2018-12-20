98348a0ee7596953462461e93f49441d14802272
Jim Alexander <alexanderj2396@gmail.com>
2018-12-20 15:40:06 +1100

    timesheet page works pretty well need to debug total hours

--- src/Pages/Timesheets/columns.js
+++ src/Pages/Timesheets/columns.js
@@ -1,168 +1,250 @@
 import moment from 'moment'
-export function timesheet(today) {
-  return [{
+
+export function timesheet(today, days) {
+  var columns = [{
     title: 'Name',
     dataIndex: 'name',
     key: 'name',
     width: 200,
-  }, {
-    title: moment(today, 'D-MMM-YYYY').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').format('D-MMM'),
-      className: 'dateSub',
-      key: 'thur',
-      width: 100
-    }]
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(1, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(1, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(1, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'fri',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(2, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(2, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(2, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'sat',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(3, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(3, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(3, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'sun',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(4, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(4, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(4, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'mon',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(5, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(5, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(5, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'tue',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(6, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(6, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(6, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'wed',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(7, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(7, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(7, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'thur2',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(8, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(8, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(8, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'fri2',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(9, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(9, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(9, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'sat2',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(10, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(10, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(10, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'sun2',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(11, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(11, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(11, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'mon2',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(12, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(12, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(12, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'tue2',
-      width: 100
-    }],
-  }, {
-    title: moment(today, 'D-MMM-YYYY').add(13, 'days').format('ddd'),
-    className: 'timesheetDay',
-    children: [{
-      title: moment(today, 'D-MMM-YYYY').add(13, 'days').format('D-MMM'),
-      dataIndex: moment(today, 'D-MMM-YYYY').add(13, 'days').format('D-MMM'),
-      className: 'dateSub',
-      key: 'wed2',
-      width: 100
-    }],
-  },{
+    sorter: (a, b) => {
+      if (a.name < b.name) { return -1; }
+      if (a.name > b.name) { return 1; }
+      return 0;
+    },
+    defaultSortOrder: 'ascending'
+  }];
+  for (let i = 0; i < days; i++) {
+    columns.push({
+      title: moment(today, 'D-MMM-YYYY').add(i, 'days').format('ddd'),
+      className: 'timesheetDay',
+      children: [{
+        title: moment(today, 'D-MMM-YYYY').add(i, 'days').format('D-MMM'),
+        dataIndex: moment(today, 'D-MMM-YYYY').add(i, 'days').format('D-MMM'),
+        className: 'dateSub',
+        key: moment(today, 'D-MMM-YYYY').add(i, 'days').format('ddd') + i,
+        width: 100
+      }],
+    })
+  }
+  columns.push({
     title: 'Hours',
     dataIndex: 'hours',
     key: 'hours',
     width: 100,
+    render: (num) => {
+      return Math.round(num * 100) / 100
+    },
     className: 'timesheetTotals'
   }, {
-    title: 'OT1',
-    dataIndex: 'ot1',
-    key: 'ot1',
-    width: 100,
-    className: 'timesheetTotals'
-  }, {
-    title: 'OT2',
-    dataIndex: 'ot2',
-    key: 'ot2',
-    width: 100,
-    className: 'timesheetTotals'
-  }]
-
+      title: 'OT1',
+      dataIndex: 'ot1',
+      key: 'ot1',
+      width: 100,
+      render: (num) => {
+        if (num === 0) {
+          return null
+        } else {
+          return num
+        }
+      },
+      className: 'timesheetTotals'
+    }, {
+      title: 'OT2',
+      dataIndex: 'ot2',
+      key: 'ot2',
+      width: 100,
+      render: (num) => {
+        if (num === 0) {
+          return null
+        } else {
+          return num
+        }
+      },
+      className: 'timesheetTotals'
+    })
+  return columns
 }
+
+// export function timesheet(today) {
+//   return [{
+//     title: 'Name',
+//     dataIndex: 'name',
+//     key: 'name',
+//     width: 200,
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'thur',
+//       width: 100
+//     }]
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(1, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(1, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(1, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'fri',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(2, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(2, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(2, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'sat',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(3, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(3, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(3, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'sun',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(4, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(4, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(4, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'mon',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(5, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(5, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(5, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'tue',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(6, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(6, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(6, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'wed',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(7, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(7, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(7, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'thur2',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(8, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(8, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(8, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'fri2',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(9, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(9, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(9, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'sat2',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(10, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(10, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(10, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'sun2',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(11, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(11, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(11, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'mon2',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(12, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(12, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(12, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'tue2',
+//       width: 100
+//     }],
+//   }, {
+//     title: moment(today, 'D-MMM-YYYY').add(13, 'days').format('ddd'),
+//     className: 'timesheetDay',
+//     children: [{
+//       title: moment(today, 'D-MMM-YYYY').add(13, 'days').format('D-MMM'),
+//       dataIndex: moment(today, 'D-MMM-YYYY').add(13, 'days').format('D-MMM'),
+//       className: 'dateSub',
+//       key: 'wed2',
+//       width: 100
+//     }],
+//   },{
+//     title: 'Hours',
+//     dataIndex: 'hours',
+//     key: 'hours',
+//     width: 100,
+//     render: (num) => {
+//       return Math.round(num * 100) / 100
+//     },
+//     className: 'timesheetTotals'
+//   }, {
+//     title: 'OT1',
+//     dataIndex: 'ot1',
+//     key: 'ot1',
+//     width: 100,
+//     render: (num) => {
+//       if (num === 0) {
+//         return null
+//       } else {
+//         return num
+//       }
+//     },
+//     className: 'timesheetTotals'
+//   }, {
+//     title: 'OT2',
+//     dataIndex: 'ot2',
+//     key: 'ot2',
+//     width: 100,
+//     render: (num) => {
+//       if (num === 0) {
+//         return null
+//       } else {
+//         return num
+//       }
+//     },
+//     className: 'timesheetTotals'
+//   }]
+// }
--- src/Pages/Timesheets/index.js
+++ src/Pages/Timesheets/index.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
-import { Table, DatePicker, Row, Col } from 'antd'
-import moment from 'moment'
+import { Table, DatePicker, Row, Col, Select} from 'antd'
+import Moment from 'moment'
+import { extendMoment } from 'moment-range'
 import * as column from './columns'
 import './index.css'
+
+const moment = extendMoment(Moment)
 
 export default class Timesheets extends Component {
   state = {
     start: moment().format('YYYY-MM-DD'),
     end: null,
     data: null,
+    noDays: 14
   }
   selectDate() {
     return (
       <DatePicker
-        style={{ width: '100%', maxWidth: 250, paddingLeft: 20 }}
+        style={{ width: '100%', maxWidth: 200, paddingLeft: 20 }}
         onChange={(date) => {
           if (date) {
             this.setState({
@@ -22,32 +26,42 @@ export default class Timesheets extends Component {
           }
         }}
         defaultValue={moment()}
-        format='Do MMMM YYYY' />
+        format='Do MMM YYYY' />
     )
   }
   dateTo() {
     return (
       <DatePicker
-        style={{ width: '100%', maxWidth: 250, paddingLeft: 20 }}
-        disabled={true}
+        style={{ width: '100%', maxWidth: 200, paddingLeft: 20 }}
         value={moment(this.state.end, 'YYYY-MM-DD')}
-        format='Do MMMM YYYY' />
+        disabled
+        format='Do MMM YYYY' />
+    )
+  }
+  selectDayNumber(){
+    return(
+      <Select style={{width: '100%', maxWidth: 200, paddingLeft: 20 }} defaultValue={14} onChange={(val) => this.setState({noDays: val})}>
+        <Select.Option key={14}>14</Select.Option>
+        <Select.Option key={7}>7</Select.Option>
+      </Select>
     )
   }
   componentDidMount() {
     this.loadTimesheet()
   }
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps.dailyPrestarts !== this.props.dailyPrestarts || prevState.start !== this.state.start || prevState.selectedName !== this.state.selectedName || prevState.timeSet !== this.state.timeSet) {
+    if (prevProps.dailyPrestarts !== this.props.dailyPrestarts ||
+       prevState.start !== this.state.start ||
+        prevState.noDays !== this.state.noDays) {
       this.setState({
         data: null,
-        end: moment(this.state.start, 'YYYY-MM-DD').add(13, 'days').format('YYYY-MM-DD')
+        end: moment(this.state.start, 'YYYY-MM-DD').add(this.state.noDays, 'days').format('YYYY-MM-DD')
       }, () => this.loadTimesheet())
     }
   }
   calcTimeDiff(startTime, endTime) {
     if (!startTime || !endTime) {
-      return '00:00'
+      return parseFloat(0)
     }
     // parse time using 24-hour clock and use UTC to prevent DST issues
     var start = moment.utc(startTime, "h.mm");
@@ -59,6 +73,56 @@ export default class Timesheets extends Component {
     // format a string result
     return parseFloat(moment.utc(+d).format('HH.mm'))
   }
+  calcOverTimeOne(startTime, endTime, day) {
+    if (startTime && endTime) {
+
+      startTime = moment(`2018-12-10 ${startTime}`, 'YYYY-MM-DD h.mm');
+      endTime = moment(`2018-12-10 ${endTime}`, 'YYYY-MM-DD h.mm');
+
+      var dayEndTime = moment(`2018-12-10 20:00`, 'YYYY-MM-DD HH:mm');
+      var overtimeOne = moment.duration(0);
+
+      if (endTime.isAfter(dayEndTime)) {
+        var time = startTime.isAfter(dayEndTime) ? startTime : dayEndTime;
+        overtimeOne = moment.duration(endTime.diff(time));
+      }
+      if (day === 'Saturday') {
+        overtimeOne = moment.duration(endTime.diff(startTime));
+      }
+      if (overtimeOne.as('hours') === 2 || overtimeOne.as('hours') > 2) {
+        return 2
+      } else {
+        return overtimeOne.as('hours')
+      }
+    } else {
+      return 0
+    }
+  }
+  calcOverTimeTwo(startTime, endTime, day) {
+    if (startTime && endTime) {
+
+      startTime = moment(`2018-12-10 ${startTime}`, 'YYYY-MM-DD h.mm');
+      endTime = moment(`2018-12-10 ${endTime}`, 'YYYY-MM-DD h.mm');
+
+      var dayEndTimeTwo = moment(`2018-12-10 22:00`, 'YYYY-MM-DD HH:mm');
+      var overtimeTwo = moment.duration(0);
+
+      if (endTime.isAfter(dayEndTimeTwo)) {
+        var time = startTime.isAfter(dayEndTimeTwo) ? startTime : dayEndTimeTwo;
+        overtimeTwo = moment.duration(endTime.diff(time));
+      }
+      if (day === 'Saturday') {
+        overtimeTwo = moment.duration(endTime.diff(startTime));
+        overtimeTwo.subtract(2, 'hours')
+      }
+      if (day === 'Sunday') {
+        overtimeTwo = moment.duration(endTime.diff(startTime));
+      }
+      return overtimeTwo.as('hours')
+    } else {
+      return 0
+    }
+  }
   loadTimesheet() {
     var data = [];
     this.props.dailyPrestarts.forEach(prestart => {
@@ -68,19 +132,37 @@ export default class Timesheets extends Component {
             var start = (entry.form_values['33d3']) ? entry.form_values['33d3'].choice_values[1] : '';
             var end = (entry.form_values['2748']) ? entry.form_values['2748'].choice_values[1] : '';
             var hoursDiff = this.calcTimeDiff(start, end);
+            var overTimeOne = (this.calcOverTimeOne(start, end, moment(prestart.form_values['80e9']).format('dddd')) !== 0) ? this.calcOverTimeOne(start, end, moment(prestart.form_values['80e9']).format('dddd')) : 0
+            var overTimeTwo = (this.calcOverTimeTwo(start, end, moment(prestart.form_values['80e9']).format('dddd')) !== 0) ? this.calcOverTimeTwo(start, end, moment(prestart.form_values['80e9']).format('dddd')) : 0
+
             var obj = {
               id: entry.id,
               name: entry.form_values[8464].choice_values[0],
             };
             const index = data.findIndex((e) => e.name === obj.name);
             if (index === -1) {
-              obj[moment(prestart.form_values['80e9']).format('D-MMM')] = hoursDiff
+              obj[moment(prestart.form_values['80e9']).format('D-MMM')] = hoursDiff;
+              obj.hours = hoursDiff;
+              obj.ot1 = overTimeOne
+              obj.ot2 = overTimeTwo
+              obj[moment(prestart.form_values['80e9']).format('Do-MMM') + '_start'] = start
+              obj[moment(prestart.form_values['80e9']).format('Do-MMM') + '_end'] = end
               data.push(obj);
             } else {
               if (data[index][moment(prestart.form_values['80e9']).format('D-MMM')]) {
-                data[index][moment(prestart.form_values['80e9']).format('D-MMM')] += hoursDiff
+                data[index][moment(prestart.form_values['80e9']).format('D-MMM')] += hoursDiff;
+                data[index].hours += hoursDiff
+                data[index].ot1 += overTimeOne
+                data[index].ot2 += overTimeTwo
+                data[index][moment(prestart.form_values['80e9']).format('Do-MMM') + '_start'] = start
+                data[index][moment(prestart.form_values['80e9']).format('Do-MMM') + '_end'] = end
               } else {
                 Object.assign(data[index], { [moment(prestart.form_values['80e9']).format('D-MMM')]: hoursDiff })
+                data[index].hours += hoursDiff
+                data[index].ot1 += overTimeOne
+                data[index].ot2 += overTimeTwo
+                data[index][moment(prestart.form_values['80e9']).format('Do-MMM') + '_start'] = start
+                data[index][moment(prestart.form_values['80e9']).format('Do-MMM') + '_end'] = end
               }
             }
           }
@@ -96,8 +178,9 @@ export default class Timesheets extends Component {
       <div>
         <div style={{ marginBottom: 10 }}>
           <Row gutter={10}>
-            <Col span={12}><h2>From: {this.selectDate()}</h2></Col>
-            <Col span={12}><h2>To: {this.dateTo()}</h2></Col>
+            <Col span={8}><h2>From: {this.selectDate()}</h2></Col>
+            <Col span={8}><h2>To: {this.dateTo()}</h2></Col>
+            <Col span={8}><h2>Days: {this.selectDayNumber()}</h2></Col>
           </Row>
 
         </div>
@@ -106,7 +189,7 @@ export default class Timesheets extends Component {
           bordered
           id='timesheet'
           className='boreTables tableResizer dailyReportTables'
-          columns={column.timesheet(moment(this.state.start).format('D-MMM-YYYY'))}
+          columns={column.timesheet(moment(this.state.start).format('D-MMM-YYYY'), this.state.noDays)}
           dataSource={this.state.data}
           rowClassName={(record) => {
             if (record.name === ' ') {
